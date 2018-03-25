83dafc2af59f337ac8ae1afcc8e0fa2df0d00878
SaiyanShivvy <hdiproductionsoffical@gmail.com>
2018-03-26 12:54:47 +1300

    rename SnakeGame.cpp to main.cpp. Added function for food collision, resetting the snake and creating the snake.

--- SnakeGame/snake.cpp
+++ SnakeGame/snake.cpp
@@ -9,5 +9,47 @@
 using namespace std;
 
 SNAKE::SNAKE() {
-
+	asnake.push_front(COORD(25, 25)); //Inital position of the snake
+	asnake.push_front(COORD(50, 25));
+	asnake.push_front(COORD(75, 25));
+	foodCount = 0;
 }
+
+//Resets the snake
+void SNAKE::reset() {
+	asnake.clear();//clears the snake
+	asnake.push_front(COORD(25, 25)); //set new snake
+	asnake.push_front(COORD(50, 25));
+	asnake.push_front(COORD(75, 25));
+	asnake.push_front(COORD(100, 25));
+	foodCount = 0; //Reset foodCount 
+}
+
+
+//Grows the Snake
+void SNAKE::grow() {
+	asnake.push_back(COORD(asnake.back().getX(), asnake.back().getY())); //the snake grows using deque function by adding a section to the end of queue
+}
+
+//checks for food collision
+bool SNAKE::foodCollide(COORD food) {
+	if (asnake.front().getX == food.getX() && asnake.front().getY() == food.getY()) { //Checks if the snake is touching the food
+		cout << "FOOD EATEN" << endl; //Debug
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+//create food
+
+
+
+
+
+
+
+
+
+
--- SnakeGame/snake.h
+++ SnakeGame/snake.h
@@ -14,7 +14,7 @@ using namespace std;
 //Declares the Snake Class
 class SNAKE {
 private:
-	enum Direction {UP, DOWN, RIGHT, LEFT}; //Game Control
+	enum Direction { UP, DOWN, RIGHT, LEFT }; //Game Control
 	std::deque<COORD> asnake; //coord's of the snake, using deque
 	int adirection; //Set default direction of snake
 
@@ -23,12 +23,12 @@ public:
 	COORD food(); //food location using COORD class
 	bool move(int); //moves the snake
 	bool touch(); //used when checking for collision of the snake colliding with itself
-	bool eat(); //used to check if the snake collides with the fruit
+	bool foodCollide(COORD food); //used to check if the snake collides with the fruit
 
 	void grow(); //grows the snake
 	void reset(); //restores snake to initial size
 
-	//int foodCount; //tracks the amount of food eaten.
+	int foodCount; //tracks the amount of food eaten.
 	int getDirection() {
 		return adirection; //returns the current direction of the snake
 	}
@@ -37,6 +37,11 @@ public:
 	int getX();
 	int getY();
 	std::deque<COORD> getCoords(); //return the coord of the snake
+
+	//setters
+	void setX(int x);
+	void setY(int y);
+	std::deque<COORD> setCoords(int x, int y);
 };
 
 
