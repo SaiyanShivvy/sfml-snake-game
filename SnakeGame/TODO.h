b60cf171e38008282353d430d46d92d281580906
SaiyanShivvy <hdiproductionsoffical@gmail.com>
2018-04-01 03:04:43 +1200

    edited some stuff, nothing impactful

--- SnakeGame/TODO.h
+++ SnakeGame/TODO.h
@@ -3,15 +3,15 @@
 
 /*
 TODO:
-	Display score
-	snake eat itself collsion //MUST
-	rainbow snake (effectively)
-
-	Improve State Machine
+	Display score -- gets logged to console
+	change the color of new rects added to snake.
+	try and get font to display
 
 MAYBE TODO:
 	2nd Player
 
 TODO COMPLETED:
 	Draw a grid
+	snake collision
+	switch state machine
 */
